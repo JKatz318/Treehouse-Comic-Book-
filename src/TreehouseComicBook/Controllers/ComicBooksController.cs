9328ba1b250ffc3cbe768353385b86a8cf1bbce3
Jeremy Katz <JKatzDDS@gmail.com>
2018-05-06 17:01:51 +0300

    Added Detail action method

--- src/TreehouseComicBook/Controllers/ComicBooksController.cs
+++ src/TreehouseComicBook/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace TreehouseComicBook.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
     }
 }
