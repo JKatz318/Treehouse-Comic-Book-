ad726fb90e489a9df71afad0308428d16ddb23d0
Jeremy Katz <JKatzDDS@gmail.com>
2018-05-06 17:52:38 +0300

    Updated Detail action method to use action result types

--- src/TreehouseComicBook/Controllers/ComicBooksController.cs
+++ src/TreehouseComicBook/Controllers/ComicBooksController.cs
@@ -10,15 +10,12 @@ namespace TreehouseComicBook.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek==DayOfWeek.Monday)
+            if (DateTime.Today.DayOfWeek==DayOfWeek.Sunday)
             {
-                return new RedirectResult("/");
+                return Redirect("/");
             }
-            return new ContentResult()
-            {
-                Content = "Hello from the comic books controller!"
-            };
-           
+            return Content("Hello from the comic books controller!");
+          
                 
             
         }
